ed9e51404b07a3810b23ab3e9fd3fa4d397ede25
Brandon Elzy <brandon.g.elzy@gmail.com>
2020-12-07 10:24:47 -0600

    Created data gen script

---
+++ server/database/dataScript.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const faker = require('faker');
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream(path.resolve(__dirname, 'data.csv'))
+writeStream.write('product name, images\n', 'utf-8');
+
+let imageNum = 0;
+for (let i = 0; i < 1000000; i++) {
+
+  const num = Math.floor(Math.random() * 5) + 1;
+  let images = '';
+  for (let j = 0; j < num; j++) {
+
+    images += j === 0
+      ? `image-${ imageNum }.jpeg`
+      : `, image-${ imageNum }.jpeg`;
+    imageNum++;
+  }
+
+  writeStream.write(`${ faker.commerce.productName() }, ${ images }\n`, 'utf-8');
+
+}
+
+writeStream.end();
